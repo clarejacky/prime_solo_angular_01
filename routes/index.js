645432161279e7cdd94ab197f4f0559effd4b811
clarejacky <clare.jacky@gmail.com>
2015-05-18 16:36:02 -0500

    routes

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var path = require('path');
-var Love = require('../models/loveNotes')
+var Love = require('../models/loveNotes');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.sendFile(path.resolve(__dirname, '../views/index.html'));
--- routes/love.js
+++ routes/love.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
+var path = require('path');
+var Love = require('../models/loveNotes');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+
+router.get("/", function(req, res, next){
+  Love.find(function(err, lovenotes){
+    res.json(lovenotes);
+  });
+});
+
+router.post("/", function(req,res,next){
+  console.log("MAde it here");
+  console.log(req.body);
+  Love.create(req.body, function(err, post){
+    res.sendFile(path.join(__dirname, '../views/index.html'));
+  })
 });
 
 module.exports = router;
