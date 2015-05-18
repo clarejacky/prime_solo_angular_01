92b823f69b8041d708d5ff73b62a65579cd721d6
clarejacky <clare.jacky@gmail.com>
2015-05-18 16:11:07 -0500

    initial commit

---
+++ client/scripts/app.js
@@ -0,0 +1,30 @@
+
+var myApp = angular.module("myApp", []);
+//controller is additional functionality that we want to add
+myApp.controller('GiveController', ['$scope', function($scope){
+    $scope.heading = "Message: ";
+
+    $scope.updateGive = function(e){
+        e.preventDefault();
+        $scope.give = true;
+        $score.giveLove = false;
+        //$scope.receive = false;
+    }
+
+    $scope.goodbyeMessage = function(){
+        $scope.message = "Goodbye, " + $scope.name + " :(";
+    }
+}]);
+
+myApp.controller('ReceiveController', ['$scope', function($scope){
+    $scope.heading = "Message: ";
+
+    $scope.updateReceive = function(){
+        $scope.receive = true;
+        //$scope.give =false;
+    }
+
+    $scope.goodbyeMessage = function(){
+        $scope.message = "Goodbye, " + $scope.name + " :(";
+    }
+}]);
